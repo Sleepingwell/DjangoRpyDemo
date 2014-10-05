ec130b29e96a465d45af5c02c118ddf024cf93c1
Simon Knapp <simon.knapp@verusol.com>
2014-10-06 01:11:51 +1100

    Support pyRserve and documentation.

--- views.py
+++ views.py
@@ -2,26 +2,36 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django import forms
+import random, os
+
+resultDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static/results/")
+rFuncStr = """
+    renderer <- function(filename, title, units) {
+        dat <- rnorm(1000)
+        png(file=filename, width=720, height=480)
+        hist(dat, main=title, xlab=units)
+        dev.off()
+    }
+"""
 
 try:
-    from rpy2.robjects import globalEnv
+    from rpy2.robjects import r as R
+
+    def getRConnection():
+        return R
 except:
-    from rpy2.robjects import globalenv as globalEnv
+    import pyRserve
+    conn = None
+    rServeHost = 'localhost'
+    rServePort = 6311
 
-from rpy2.robjects import r as R
-
-import random
-
-resultDir = "static/results/"
-
-# might be better call source on a file or load a package here.
-R("""renderer <- function(filename, title, units) {
-    dat <- rnorm(1000)
-    png(file=filename, width=720, height=480)
-    hist(dat, main=title, xlab=units)
-    dev.off()
-}""")
-
+    def getRConnection():
+        global conn
+        if conn and type(conn) is pyRserve.rconn.RConnector and not conn.isClosed:
+            return conn.r
+        conn = pyRserve.connect(host=rServeHost, port=6311)
+        conn.eval(rFuncStr)
+        return conn.r
 
 def index(request):
     return render_to_response('index.html', {'form': MainForm()}, context_instance=RequestContext(request))
@@ -48,5 +58,6 @@ def doFDC(request):
     (siteId, siteName) = request.GET["value"].split("|")
     (seriesType, units) = (request.COOKIES["seriesType"], request.COOKIES["units"])
     filename = "%d_%s" % (random.randint(1000, 9999), "boxplot.png")
+    R = getRConnection()
     R.renderer(resultDir + filename, "Random data for site %s (%s)" % (siteName, units), units)
     return HttpResponse('<img src="/static/results/' + filename + '" />')
